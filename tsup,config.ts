8997b106336642ed2b83c13cba721f667415bc8f
Atha Ardisa <92588716+ardisaatha@users.noreply.github.com>
2025-09-04 14:24:17 +0700

    press size lib

--- tsup,config.ts
+++ tsup,config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  splitting: false,
-  sourcemap: true,
+  splitting: true,
+  sourcemap: false,
   clean: true,
   dts: true,
   format: ["esm", "cjs"],
   minify: true,
   target: "es2020",
   external: ["react", "react-dom"],
-  loader: { ".css": "css" },
 });
