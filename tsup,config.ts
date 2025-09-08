1f149a793e9fa4e7f9e2d58072b8f9a8fd359a5c
Atha Ardisa <92588716+ardisaatha@users.noreply.github.com>
2025-09-08 10:55:09 +0700

    add postcss on tsup file

--- tsup,config.ts
+++ tsup,config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from "tsup";
+import postcss from "postcss";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import fs from "fs/promises";
 
 export default defineConfig({
   entry: ["src/index.ts"],
@@ -10,4 +14,12 @@ export default defineConfig({
   minify: true,
   target: "es2020",
   external: ["react", "react-dom"],
+  async onSuccess() {
+    const css = await fs.readFile("src/components/global.css", "utf-8");
+    const result = await postcss([tailwindcss, autoprefixer]).process(css, {
+      from: "src/components/global.css",
+      to: "dist/index.css",
+    });
+    await fs.writeFile("dist/index.css", result.css);
+  },
 });
